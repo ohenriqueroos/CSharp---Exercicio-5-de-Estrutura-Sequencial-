01752bd34383505c2d744a9b17617a11c8b70a0c
henri <henri@HENRIQUE>
2024-04-18 23:06:55 -0300

    Add project files.

---
+++ SequentialStructureExercise5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace SequentialStructureExercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] part1 = Console.ReadLine().Split(" ");
+            string[] part2 = Console.ReadLine().Split(" ");
+
+            double totalPart1 = double.Parse(part1[1], CultureInfo.InvariantCulture) * double.Parse(part1[2], CultureInfo.InvariantCulture);
+            double totalPart2 = double.Parse(part2[1], CultureInfo.InvariantCulture) * double.Parse(part2[2], CultureInfo.InvariantCulture);
+
+            double totalPayment = totalPart1 + totalPart2;
+
+            Console.WriteLine("VALOR A PAGAR: R$" + totalPayment.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
